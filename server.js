4d24d501ecc8ef91f9152a3230eae907cd4fd76e
Selaelo Sebola <selaelosebola67@gmail.com>
2021-09-30 22:39:19 +0200

    minor modifaction on server.js and installed nodemon

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+const db = knex({
+   client: 'pg',
+   connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: '',
+      database: 'smart-brain-database',
+   }
+});
 
 const app = express();
 
@@ -50,13 +61,11 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
    const { email, name, password } = req.body;
-   database.users.push({
-      id: '125',
-       name: name,
-       email: email,
-       entries: 0,
-       joined: new Date()
-   })
+   db('users').insert({
+      email: email,
+      name: name,
+      joined: new Date(),
+   }).then(console.log)
    res.json(database.users[database.users.length-1]);
 })
 
